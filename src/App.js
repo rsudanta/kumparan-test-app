6ac4e9e14d0d4754ab395f950f14ea64103a4acb
Danta <rsudanta20@gmail.com>
2022-01-18 15:15:38 +0700

    Project Init

---
+++ src/App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView, StyleSheet,
+  Text,
+  useColorScheme
+} from 'react-native';
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hallo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
